9a2fe1623567a116026d46dbaa827608861e2467
Dogel-ai <dogel.kszb@proton.me>
2025-02-18 22:50:17 +0000

    fix: secrets being blank values

--- lib/spotify_liked_to_playlist.ex
+++ lib/spotify_liked_to_playlist.ex
@@ -1,14 +1,16 @@
 defmodule SpotifyLikedToPlaylist do
-  @client_id System.get_env("CLIENT_ID")
-  @client_secret System.get_env("CLIENT_SECRET")
+  def secrets(), do: {System.get_env("CLIENT_ID"), System.get_env("CLIENT_SECRET")}
 
   def access_token() do
+    {client_id, client_secret} = secrets()
+    IO.puts(client_id)
+    IO.puts(client_secret)
     {:ok, _} = Finch.start_link(name: MyFinch)
     req = Finch.build(
       :post,
       "https://accounts.spotify.com/api/token",
       [{"Content-Type", "application/x-www-form-urlencoded"}],
-      "grant_type=client_credentials&client_id=#{@client_id}&client_secret=#{@client_secret}"
+      "grant_type=client_credentials&client_id=#{client_id}&client_secret=#{client_secret}"
     )
     {:ok, resp} = Finch.request(req, MyFinch)
 
