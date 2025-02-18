3a93b360e6d46f9a7a876676f6bb849cb37c6549
Dogel-ai <dogel.kszb@proton.me>
2025-02-18 19:01:04 +0000

    batman

---
+++ lib/spotify_liked_to_playlist.ex
@@ -0,0 +1,18 @@
+defmodule SpotifyLikedToPlaylist do
+  @moduledoc """
+  Documentation for `SpotifyLikedToPlaylist`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpotifyLikedToPlaylist.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/spotify_liked_to_playlist_test.exs
@@ -0,0 +1,8 @@
+defmodule SpotifyLikedToPlaylistTest do
+  use ExUnit.Case
+  doctest SpotifyLikedToPlaylist
+
+  test "greets the world" do
+    assert SpotifyLikedToPlaylist.hello() == :world
+  end
+end
