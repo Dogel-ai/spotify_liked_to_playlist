ad5ace12dafcbdea17f43121e9cd095b0163a52d
Dogel-ai <dogel.kszb@proton.me>
2025-02-18 22:00:23 +0000

    feat: add access token fetch

--- lib/spotify_liked_to_playlist.ex
+++ lib/spotify_liked_to_playlist.ex
@@ -1,51 +1,22 @@
 defmodule SpotifyLikedToPlaylist do
   @client_id System.get_env("CLIENT_ID")
   @client_secret System.get_env("CLIENT_SECRET")
-  @base_url Req.new(base_url: "https://api.spotify.com/v1")
-  @moduledoc """
-  Documentation for `SpotifyLikedToPlaylist`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SpotifyLikedToPlaylist.hello()
-      :world
-
-  """
   def access_token() do
-    # resp = Req.post!(
-    #   url: "https://accounts.spotify.com/api/token",
-    #   headers: "Content-Type: application/x-www-form-urlencoded",
-    #   body: "grant_type=client_credentials&client_id=#{@client_id}&client_secret=#{@client_secret}"
-    # )
+    {:ok, _} = Finch.start_link(name: MyFinch)
+    req = Finch.build(
+      :post,
+      "https://accounts.spotify.com/api/token",
+      [{"Content-Type", "application/x-www-form-urlencoded"}],
+      "grant_type=client_credentials&client_id=#{@client_id}&client_secret=#{@client_secret}"
+    )
+    {:ok, resp} = Finch.request(req, MyFinch)
 
-    req =
-      Req.Request.new(
-        method: :post,
-        url: "https://httpbin.org/post",
-        # headers: "Content-Type: application/x-www-form-urlencoded",
-        body: "grant_type=client_credentials&client_id=#{@client_id}&client_secret=#{@client_secret}"
-      )
-      |> Req.Request.append_request_steps(
-        put_user_agent: &Req.Steps.put_user_agent/1
-      )
-      |> Req.Request.append_response_steps(
-        decompress_body: &Req.Steps.decompress_body/1,
-        decode_body: &Req.Steps.decode_body/1
-      )
-      |> Req.Request.append_error_steps(
-        retry: &Req.Steps.retry/1
-      )
-
-    {req, resp} = Req.Request.run_request(req)
-    
-    req
-    # cond do
-    #   resp.status == 200 -> resp.body["access_token"]
-    #   true -> raise "expected status 200, got #{resp.status}"
-    # end
+    cond do
+      resp.status == 200 -> resp.body
+        |> Jason.decode!()
+        |> Map.get("access_token")
+      true -> raise "expected code 200, got #{resp.status}"
+    end
   end
 end
